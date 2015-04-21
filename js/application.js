118ea26f434d09b856716ccedbedf82a3ddabdd8
chendesheng <ichendesheng@gmail.com>
2015-04-21 08:39:03 +0800

    Start a naive AI

--- js/application.js
+++ js/application.js
@@ -1,4 +1,16 @@
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
-  new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+	var game = new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+
+	var onemove = function() {
+		if(localStorage.gameState) {
+			var state = JSON.parse(localStorage.gameState);
+			if (state.over) return;
+
+			game.move(Math.ceil(Math.random()*10)%4);
+			setTimeout(onemove, 100);
+		}
+	}
+
+	onemove();
 });
