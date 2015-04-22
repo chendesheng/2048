c31fd78e02ab793ab32c4640b562063b1acad948
chendesheng <ichendesheng@gmail.com>
2015-04-22 22:00:18 +0800

    Keep playing automatically

--- js/application.js
+++ js/application.js
@@ -3,20 +3,25 @@ window.requestAnimationFrame(function () {
 	var game = new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
 
 	window.move = function(d) {
+		if (game.isGameTerminated()) {
+			game.keepPlaying = true;
+			setTimeout(function() {
+				document.querySelector(".game-message").classList.remove("game-won")
+			}, 100);
+		}
+
 		game.move(d);
 
 		if(localStorage.gameState) {
 			var state = JSON.parse(localStorage.gameState);
 			if (state.over) return;
 
-			setTimeout(function() {
-				var e = document.createElement('script');
-				e.type = 'text/javascript';
-				e.async = true;
-				e.src = 'http://localhost:8877?'+encodeURIComponent(JSON.stringify(state.grid));
-				var s = document.getElementsByTagName('script')[0];
-				s.parentNode.insertBefore(e, s);
-			}, 0);
+			var e = document.createElement('script');
+			e.type = 'text/javascript';
+			e.async = true;
+			e.src = 'http://localhost:8877?'+encodeURIComponent(JSON.stringify(state.grid));
+			var s = document.getElementsByTagName('script')[0];
+			s.parentNode.insertBefore(e, s);
 		}
 	}
 
